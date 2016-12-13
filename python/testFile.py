ea1c8eb3d34a000d2ef3c065a2bb1b0d8928eab8
David Moneysmith <moneysmith777@gmail.com>
2016-12-13 05:32:20 -0800

    Enabled server clustering

--- python/testFile.py
+++ python/testFile.py
@@ -3,9 +3,9 @@ import random
 print 'Hello, world!'
 
 def main():
-    print 'In main'
-    for i in range(5):
-        print i, random.randrange(0, 10)
-    print 'Done.'
+  print 'In main'
+  for i in range(5):
+    print i, random.randrange(0, 10)
+  print 'Done.'
 
 main()
